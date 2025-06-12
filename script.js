096681af783156a011ecd90f014f02942fff28eb
Alexandre Kranz <65457837+akranz79@users.noreply.github.com>
2025-06-12 16:21:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Comportamento suave de rolagem
+document.querySelectorAll("nav a").forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
